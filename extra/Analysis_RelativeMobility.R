4b3581f8a96c6d55944ac89c7d7b526e1409a6a7
Serene <luckyseorin@gmail.com>
2023-06-29 13:20:12 +0200

    Plots

--- extra/Analysis_RelativeMobility.R
+++ extra/Analysis_RelativeMobility.R
@@ -69,17 +69,23 @@ s3 <- max_parents(s3)
 s4 <- max_parents(s4)
 s5 <- max_parents(s5)
 
-s1$Mother_Cultural[69]
-s1$Father_Cultural[69]
-s1$Max_Cult_P[69]
+s1$sMax_Cult_P <- scale(s1$Max_Cult_P, center = TRUE, scale = TRUE)
+s2$sMax_Cult_P <- scale(s2$Max_Cult_P, center = TRUE, scale = TRUE)
+s3$sMax_Cult_P <- scale(s3$Max_Cult_P, center = TRUE, scale = TRUE)
+s4$sMax_Cult_P <- scale(s4$Max_Cult_P, center = TRUE, scale = TRUE)
+s5$sMax_Cult_P <- scale(s5$Max_Cult_P, center = TRUE, scale = TRUE)
 
-
-s1$Max_Parents
-head(s3)
-
+s1$sCultural <- scale(s1$Cultural, center = TRUE, scale = TRUE)
+s2$sCultural <- scale(s2$Cultural, center = TRUE, scale = TRUE)
+s3$sCultural <- scale(s3$Cultural, center = TRUE, scale = TRUE)
+s4$sCultural <- scale(s4$Cultural, center = TRUE, scale = TRUE)
+s5$sCultural <- scale(s5$Cultural, center = TRUE, scale = TRUE)
+# scaling is possible but then interpretation becomes harder and it deosn't seem necessary given the same scales. 
+# Also, there's not much difference after plotting the coefficient regression (because it anyway scales a bit).
+unique(s1$sCultural)
+unique(s1$Cultural)
 
 # Per Cohort --------------------------------------------------------------
-
 
 # Test for Max_Parents ----------------------------------------------------
 "it does not seem to be a good idea to use Max_Parents when
@@ -127,27 +133,53 @@ ctable$Cohort <- 3
 ctable
 output_df <- cbind(output_df, ctable)
 
+
 # Regression Coefficients per Cohort + Rep -------------------------------------
+## Linear With Cultural -----------------------------------------------------------
+#  With Interaction --> Hard to interpret!! 
+# calculate_coefficients <- function(input_data) {
+#   output_df <- data.frame(matrix(ncol = 6, nrow = 0))
+#   colnames(output_df) <- c("Intercept", "Mother_Edu", "Father_Edu", "Interaction", 
+#                            "Replication", "Cohort")
+#   
+#   for (i in 1:10){
+#     df = input_data %>% filter(Replication == i)
+#     for (j in unique(df$Cohort)){
+#       df2 = df %>% filter(Cohort == j)
+#       lm = lm(df2$Cultural~df2$Mother_Cultural*df2$Father_Cultural)
+#       coef_df <- data.frame(t(data.frame(lm$coefficients)))  # Transpose the coefficients dataframe
+#       extra <- data.frame(coef(summary(lm)))
+#       colnames(extra) <- c("Estimate", "SE", "t_stat", "p_value")
+#       extra.t <- data.frame(Intercept_SE = extra$SE[1], Mother_SE = extra$SE[2], Father_SE = extra$SE[3], Interaction_SE = extra$SE[4],
+#                             Intercept_pr = extra$p_value[1], Mother_pr =extra$p_value[2], Father_pr = extra$p_value[3], Interaction_pr = extra$p_value[4])
+#       colnames(coef_df) <- c("Intercept", "Mother_Edu", "Father_Edu", "Interaction")
+#       coef_df$Replication <- i  
+#       coef_df$Cohort <- j  
+#       binding <- cbind(coef_df, extra.t)
+#       output_df <- rbind(output_df, binding)
+#     }
+#   }
+#   return(output_df)
+# }
 
 
-# Linear Model (Not Correct?) ---------------------------------------------
+## Linear With Max_Cult_P ---------------------------------------------------------
 
 calculate_coefficients <- function(input_data) {
-  output_df <- data.frame(matrix(ncol = 6, nrow = 0))
-  colnames(output_df) <- c("Intercept", "Mother_Edu", "Father_Edu", "Interaction", 
+  output_df <- data.frame(matrix(ncol = 4, nrow = 0))
+  colnames(output_df) <- c("Intercept", "Parents_Edu", 
                            "Replication", "Cohort")
-  
   for (i in 1:10){
     df = input_data %>% filter(Replication == i)
     for (j in unique(df$Cohort)){
       df2 = df %>% filter(Cohort == j)
-      lm = lm(df2$Edu_level~df2$Mother_Edu*df2$Father_Edu) # 2 main effects & 1 interaction effect
+      lm = lm(df2$Cultural~df2$Max_Cult_P)
       coef_df <- data.frame(t(data.frame(lm$coefficients)))  # Transpose the coefficients dataframe
       extra <- data.frame(coef(summary(lm)))
       colnames(extra) <- c("Estimate", "SE", "t_stat", "p_value")
-      extra.t <- data.frame(Intercept_SE = extra$SE[1], Mother_SE = extra$SE[2], Father_SE = extra$SE[3], Interaction_SE = extra$SE[4],
-                            Intercept_pr = extra$p_value[1], Mother_pr =extra$p_value[2], Father_pr = extra$p_value[3], Interaction_pr = extra$p_value[4])
-      colnames(coef_df) <- c("Intercept", "Mother_Edu", "Father_Edu", "Interaction")
+      extra.t <- data.frame(Intercept_SE = extra$SE[1], Parents_SE = extra$SE[2],
+                            Intercept_pr = extra$p_value[1], Parents_pr =extra$p_value[2])
+      colnames(coef_df) <- c("Intercept", "Parents_Edu")
       coef_df$Replication <- i  
       coef_df$Cohort <- j  
       binding <- cbind(coef_df, extra.t)
@@ -157,36 +189,7 @@ calculate_coefficients <- function(input_data) {
   return(output_df)
 }
 
-
-# With Cultural -----------------------------------------------------------
-
-calculate_coefficients <- function(input_data) {
-  output_df <- data.frame(matrix(ncol = 6, nrow = 0))
-  colnames(output_df) <- c("Intercept", "Mother_Edu", "Father_Edu", "Interaction", 
-                           "Replication", "Cohort")
-  
-  for (i in 1:10){
-    df = input_data %>% filter(Replication == i)
-    for (j in unique(df$Cohort)){
-      df2 = df %>% filter(Cohort == j)
-      lm = lm(df2$Cultural~df2$Mother_Cultural*df2$Father_Cultural)
-      coef_df <- data.frame(t(data.frame(lm$coefficients)))  # Transpose the coefficients dataframe
-      extra <- data.frame(coef(summary(lm)))
-      colnames(extra) <- c("Estimate", "SE", "t_stat", "p_value")
-      extra.t <- data.frame(Intercept_SE = extra$SE[1], Mother_SE = extra$SE[2], Father_SE = extra$SE[3], Interaction_SE = extra$SE[4],
-                            Intercept_pr = extra$p_value[1], Mother_pr =extra$p_value[2], Father_pr = extra$p_value[3], Interaction_pr = extra$p_value[4])
-      colnames(coef_df) <- c("Intercept", "Mother_Edu", "Father_Edu", "Interaction")
-      coef_df$Replication <- i  
-      coef_df$Cohort <- j  
-      binding <- cbind(coef_df, extra.t)
-      output_df <- rbind(output_df, binding)
-    }
-  }
-  return(output_df)
-}
-
-
-# Ordinal Logistic Regression (Not working well) --------------------------
+# cfr. Ordinal Logistic Regression With Edu (Not working well) --------------------------
 
 calculate_coefficients2 <- function(input_data) {
   output_df <- data.frame()
@@ -239,151 +242,101 @@ result_s1$Scenario = 1
 result_s2$Scenario = 2
 result_s3$Scenario = 3
 df <- rbind(result_s1, result_s2, result_s3)
-head(df)
+df$label <- ifelse(df$Scenario == 1, "Scenario 1", ifelse(df$Scenario == 2, "Scenario 2", "Scenario 3"))
 
 result_s4$Scenario = 4
 result_s5$Scenario = 5
-df <- rbind(result_s4, result_s5)
+df_ex <- rbind(result_s4, result_s5)
+df_ex$label <- ifelse(df_ex$Scenario == 4, "True Homogamy", "True Heterogamy")
+
+plot(result_s4$Parents_pr ~ result_s4$Cohort)
+plot(result_s5$Parents_pr ~ result_s5$Cohort)
+plot(result_s4$Parents_SE ~ result_s4$Cohort)
+plot(result_s5$Parents_SE ~ result_s5$Cohort)
 
 
-# Create the plots
+setwd("/Users/serenekim/Desktop/Thesis_SeorinKim/MasterThesis_Seorin_Kim/images")
+png(filename = "coef_scens.png", width = 800, res = 100)
+ggplot(df, mapping = aes(x = Cohort, color = label)) +
+  geom_smooth(aes(y = Parents_Edu), method ="loess") +
+  geom_point(aes(y = Parents_Edu, alpha = 0.1)) +
+  facet_wrap(.~ label, nrow = 3) +
+  labs(x = "Cohort", y = "Parents") +
+  theme_light() + theme(legend.position="none", strip.text.x = element_text(size = 10, colour = "black"))+
+  theme(strip.background =element_rect(fill="#e6e5e3"))+ 
+  scale_color_manual(values=c('#636EFA', '#EF553B', '#00CC96'))
+dev.off()
 
-ggplot(df, aes(x = Cohort)) +
-  geom_smooth(aes(y = Mother_Edu), method ="loess", color = "blue") +
-  geom_point(aes(y = Mother_Edu)) +
-  facet_wrap(~ Scenario, nrow = 3) +
-  labs(x = "Cohort", y = "Mother") +
-  theme_minimal()
+png(filename = "coef_scens_ex.png", width = 800, res = 100)
+ggplot(df_ex, aes(x = Cohort, color = label)) +
+  geom_smooth(aes(y = Parents_Edu), method ="loess") +
+  geom_point(aes(y = Parents_Edu, alpha = 0.1)) +
+  facet_wrap(~ label, nrow = 3) +
+  labs(x = "Cohort", y = "Parents") +
+  theme_light() + theme(legend.position="none", strip.text.x = element_text(size = 10, colour = "black"))+
+  theme(strip.background =element_rect(fill="#e6e5e3"))+ 
+  scale_color_manual(values=c('#636EFA', '#EF553B', '#00CC96'))
+dev.off()
 
 
-ggplot(df, aes(x = Cohort)) +
-  geom_smooth(aes(y = Father_Edu), method ="loess", color = "red") +
-  geom_point(aes(y = Father_Edu)) +
-  facet_wrap(~ Scenario, nrow = 3) +
-  labs(x = "Cohort", y = "Father") +
-  theme_minimal()
-
-ggplot(df, aes(x = Cohort)) +
-  geom_smooth(aes(y = Interaction), method = "loess", color = "green") +
-  geom_point(aes(y = Interaction)) +
-  facet_wrap(~ Scenario, nrow = 3) +
-  labs(x = "Cohort", y = "Mother*Father") +
-  theme_minimal()
-
-
+png(filename = "SE_scens_ex.png", width = 800, res = 100)
+ggplot(df_ex, aes(x = Cohort, color = label)) +
+  geom_smooth(aes(y = Parents_SE), method ="loess") +
+  geom_point(aes(y = Parents_SE, alpha = 0.1)) +
+  facet_wrap(~ label, ncol = 2) +
+  labs(x = "Cohort", y = "Standard Error") +
+  theme_light() + theme(legend.position="none", strip.text.x = element_text(size = 10, colour = "black"))+
+  theme(strip.background =element_rect(fill="#e6e5e3"))+ 
+  scale_color_manual(values=c('#636EFA', '#EF553B', '#00CC96'))
+dev.off()
 
 # Zoomed Plots (Reg Coeff) ------------------------------------------------------------
-
-## Mother
 ggplot(result_s1, aes(x = Cohort)) +
-  geom_smooth(aes(y = Mother_Edu), method = "loess") +
-  geom_point(aes(y = Mother_Edu, alpha=0.5)) +
-  labs(x = "Cohort", y = "Mother") +
-  facet_zoom( ylim = c(-3, 6)) +
+  geom_smooth(aes(y = Parents_Edu), method = "loess") +
+  geom_point(aes(y = Parents_Edu, alpha=0.5)) +
+  labs(x = "Cohort", y = "Parents") +
+  facet_zoom( ylim = c(-1, 1)) +
   theme(legend.position="none")
 
 ggplot(result_s2, aes(x = Cohort)) +
-  geom_smooth(aes(y = Mother_Edu), method = "loess") +
-  geom_point(aes(y = Mother_Edu, alpha=0.5)) +
-  labs(x = "Cohort", y = "Mother") +
-  facet_zoom( ylim = c(-3, 6)) +
+  geom_smooth(aes(y = Parents_Edu), method = "loess") +
+  geom_point(aes(y = Parents_Edu, alpha=0.5)) +
+  labs(x = "Cohort", y = "Parents") +
+  facet_zoom( ylim = c(-1, 1)) +
   theme(legend.position="none")
 
 ggplot(result_s3, aes(x = Cohort)) +
-  geom_smooth(aes(y = Mother_Edu), method = "loess") +
-  geom_point(aes(y = Mother_Edu, alpha=0.5)) +
-  labs(x = "Cohort", y = "Mother") +
-  facet_zoom( ylim = c(-3, 6)) +
+  geom_smooth(aes(y = Parents_Edu), method = "loess") +
+  geom_point(aes(y = Parents_Edu, alpha=0.5)) +
+  labs(x = "Cohort", y = "Parents") +
+  facet_zoom( ylim = c(-1, 1)) +
   theme(legend.position="none")
 
+
+## Extreme Cases
+png(filename = "Zcoef_homo.png", width = 800, res = 100)
 ggplot(result_s4, aes(x = Cohort)) +
-  geom_smooth(aes(y = Mother_Edu), method = "loess") +
-  geom_point(aes(y = Mother_Edu, alpha=0.5)) +
-  labs(x = "Cohort", y = "Mother") +
-  facet_zoom( ylim = c(-3, 6)) +
-  theme(legend.position="none")
+  geom_smooth(aes(y = Parents_Edu), method = "loess", color = "#EF553B") +
+  geom_point(aes(y = Parents_Edu, alpha=0.5), color = "#EF553B") +
+  labs(x = "Cohort", y = "Parents", title = "True Homogamy") +
+  facet_zoom( ylim = c(-1, 1)) + 
+  theme_light() + theme(legend.position="none") 
+dev.off()
 
+png(filename = "Zcoef_hetero.png", width = 800, res = 100)
 ggplot(result_s5, aes(x = Cohort)) +
-  geom_smooth(aes(y = Mother_Edu), method = "loess") +
-  geom_point(aes(y = Mother_Edu, alpha=0.5)) +
-  labs(x = "Cohort", y = "Mother") +
-  facet_zoom( ylim = c(-3, 6)) +
-  theme(legend.position="none")
+  geom_smooth(aes(y = Parents_Edu), method = "loess", color = '#636EFA') +
+  geom_point(aes(y = Parents_Edu, alpha=0.5), color = '#636EFA') +
+  labs(x = "Cohort", y = "Parents", title = "True Heterogamy") +
+  facet_zoom( ylim = c(-1, 1)) +
+  theme_light() + theme(legend.position="none") 
+dev.off()
 
-## Father
-ggplot(result_s1, aes(x = Cohort)) +
-  geom_smooth(aes(y = Father_Edu), method = "loess") +
-  geom_point(aes(y = Father_Edu, alpha=0.5)) +
-  labs(x = "Cohort", y = "Father") +
-  facet_zoom( ylim = c(-3, 6)) +
-  theme(legend.position="none")
-
-ggplot(result_s2, aes(x = Cohort)) +
-  geom_smooth(aes(y = Father_Edu), method = "loess") +
-  geom_point(aes(y = Father_Edu, alpha=0.5)) +
-  labs(x = "Cohort", y = "Father") +
-  facet_zoom( ylim = c(-3, 6)) +
-  theme(legend.position="none")
-
-ggplot(result_s3, aes(x = Cohort)) +
-  geom_smooth(aes(y = Father_Edu), method = "loess") +
-  geom_point(aes(y = Father_Edu, alpha=0.5)) +
-  labs(x = "Cohort", y = "Father") +
-  facet_zoom( ylim = c(-3, 6)) +
-  theme(legend.position="none")
-
-ggplot(result_s4, aes(x = Cohort)) +
-  geom_smooth(aes(y = Father_Edu), method = "loess") +
-  geom_point(aes(y = Father_Edu, alpha=0.5)) +
-  labs(x = "Cohort", y = "Father") +
-  facet_zoom( ylim = c(-3, 6)) +
-  theme(legend.position="none")
-
-ggplot(result_s5, aes(x = Cohort)) +
-  geom_smooth(aes(y = Father_Edu), method = "loess") +
-  geom_point(aes(y = Father_Edu, alpha=0.5)) +
-  labs(x = "Cohort", y = "Father") +
-  facet_zoom( ylim = c(-3, 6)) +
-  theme(legend.position="none")
-
-## Interaction
-ggplot(result_s1, aes(x = Cohort)) +
-  geom_smooth(aes(y = Interaction), method = "loess") +
-  geom_point(aes(y = Interaction, alpha=0.5)) +
-  labs(x = "Cohort", y = "Interaction") +
-  facet_zoom( ylim = c(-5, 6)) +
-  theme(legend.position="none")
-
-ggplot(result_s2, aes(x = Cohort)) +
-  geom_smooth(aes(y = Interaction), method = "loess") +
-  geom_point(aes(y = Interaction, alpha=0.5)) +
-  labs(x = "Cohort", y = "Interaction") +
-  facet_zoom( ylim = c(-5, 6)) +
-  theme(legend.position="none")
-
-ggplot(result_s3, aes(x = Cohort)) +
-  geom_smooth(aes(y = Interaction), method = "loess") +
-  geom_point(aes(y = Interaction, alpha=0.5)) +
-  labs(x = "Cohort", y = "Interaction") +
-  facet_zoom( ylim = c(-5, 6)) +
-  theme(legend.position="none")
-
-ggplot(result_s4, aes(x = Cohort)) +
-  geom_smooth(aes(y = Interaction), method = "loess") +
-  geom_point(aes(y = Interaction, alpha=0.5)) +
-  labs(x = "Cohort", y = "Interaction") +
-  facet_zoom( ylim = c(-5, 6)) +
-  theme(legend.position="none")
-
-ggplot(result_s5, aes(x = Cohort)) +
-  geom_smooth(aes(y = Interaction), method = "loess") +
-  geom_point(aes(y = Interaction, alpha=0.5)) +
-  labs(x = "Cohort", y = "Interaction") +
-  facet_zoom( ylim = c(-5, 6)) +
-  theme(legend.position="none")
 
 
 # Ranking per Cohort + Rep -----------------------------------------------------------------
+
+# Higher ranking = higher value
 
 calculate_meanrank <- function(input_data) {
   output_df <- data.frame(matrix(ncol = 5, nrow = 0))
@@ -419,19 +372,18 @@ result2_s2 <- calculate_meanrank(s2)
 result2_s3 <- calculate_meanrank(s3)
 result2_s4 <- calculate_meanrank(s4)
 result2_s5 <- calculate_meanrank(s5)
-head(result2_s1)
 
-plot(result2_s1$Mean_Perc ~ result_s1$Cohort) 
-plot(result2_s2$Mean_Perc ~ result_s2$Cohort) 
-plot(result2_s3$Mean_Perc ~ result_s3$Cohort) 
-
-min(result2_s1$Mean_Perc, na.rm = T)
-min(result2_s2$Mean_Perc, na.rm = T)
-min(result2_s3$Mean_Perc, na.rm = T)
-
-median(result2_s1$Mean_Perc, na.rm = T)
-median(result2_s2$Mean_Perc, na.rm = T)
-median(result2_s3$Mean_Perc, na.rm = T)
+# plot(result2_s1$Mean_Perc ~ result_s1$Cohort) 
+# plot(result2_s2$Mean_Perc ~ result_s2$Cohort) 
+# plot(result2_s3$Mean_Perc ~ result_s3$Cohort) 
+# 
+# min(result2_s1$Mean_Perc, na.rm = T)
+# min(result2_s2$Mean_Perc, na.rm = T)
+# min(result2_s3$Mean_Perc, na.rm = T)
+# 
+# median(result2_s1$Mean_Perc, na.rm = T)
+# median(result2_s2$Mean_Perc, na.rm = T)
+# median(result2_s3$Mean_Perc, na.rm = T)
 
 
 
@@ -443,26 +395,39 @@ median(result2_s3$Mean_Perc, na.rm = T)
 # )
 
 df2 <- rbind(
-  data.frame(Scenario = 1, Cohort = result2_s1$Cohort, Mean_Rank = result2_s1$mean.rk., Var_Rank = result2_s1$var.rk., Total = result2_s1$n, Perc = result2_s1$mean.rk./result2_s1$n),
-  data.frame(Scenario = 2, Cohort = result2_s2$Cohort, Mean_Rank = result2_s2$mean.rk., Var_Rank = result2_s2$var.rk., Total = result2_s2$n, Perc = result2_s2$mean.rk./result2_s2$n),
-  data.frame(Scenario = 3, Cohort = result2_s3$Cohort, Mean_Rank = result2_s3$mean.rk., Var_Rank = result2_s3$var.rk., Total = result2_s3$n, Perc = result2_s3$mean.rk./result2_s3$n)
+  data.frame(Scenario = "Scenario 1", Cohort = result2_s1$Cohort, Mean_Rank = result2_s1$mean.rk., Var_Rank = result2_s1$var.rk., Total = result2_s1$n, Perc = result2_s1$mean.rk./result2_s1$n),
+  data.frame(Scenario = "Scenario 2", Cohort = result2_s2$Cohort, Mean_Rank = result2_s2$mean.rk., Var_Rank = result2_s2$var.rk., Total = result2_s2$n, Perc = result2_s2$mean.rk./result2_s2$n),
+  data.frame(Scenario = "Scenario 3", Cohort = result2_s3$Cohort, Mean_Rank = result2_s3$mean.rk., Var_Rank = result2_s3$var.rk., Total = result2_s3$n, Perc = result2_s3$mean.rk./result2_s3$n)
 )
 
-df2 <- rbind(
-  data.frame(Scenario = 4, Cohort = result2_s4$Cohort, Mean_Rank = result2_s4$mean.rk., Var_Rank = result2_s4$var.rk., Total = result2_s4$n, Perc = result2_s4$mean.rk./result2_s4$n),
-  data.frame(Scenario = 5, Cohort = result2_s5$Cohort, Mean_Rank = result2_s5$mean.rk., Var_Rank = result2_s5$var.rk., Total = result2_s5$n, Perc = result2_s5$mean.rk./result2_s5$n)
+df2_ex <- rbind(
+  data.frame(Scenario = "True Homogamy", Cohort = result2_s4$Cohort, Mean_Rank = result2_s4$mean.rk., Var_Rank = result2_s4$var.rk., Total = result2_s4$n, Perc = result2_s4$mean.rk./result2_s4$n),
+  data.frame(Scenario = "True Heterogamy", Cohort = result2_s5$Cohort, Mean_Rank = result2_s5$mean.rk., Var_Rank = result2_s5$var.rk., Total = result2_s5$n, Perc = result2_s5$mean.rk./result2_s5$n)
 )
-
 
 
 # Create the plots (With Percentiles)
-ggplot(df2, aes(x = Cohort)) +
-  geom_smooth(aes(y = Perc), method = "loess", color = "blue", se = TRUE) +
-  geom_point(aes(y = Perc)) +
+png(filename = "rank_scens.png", width = 800, res = 100)
+ggplot(df2, mapping = aes(x = Cohort, color = Scenario)) +
+  geom_smooth(aes(y = Perc), method = "loess", se = TRUE) +
+  geom_point(aes(y = Perc, alpha = 0.1)) +
   facet_wrap(~ Scenario, ncol = 3) +
   labs(x = "Cohort", y = "Percentile of the child rankings") +
-  theme_minimal()
+  theme_light() + theme(legend.position="none", strip.text.x = element_text(size = 10, colour = "black"))+
+  theme(strip.background =element_rect(fill="#e6e5e3"))+ 
+  scale_color_manual(values=c('#636EFA', '#EF553B', '#00CC96'))
+dev.off()
 
+png(filename = "rank_scens_ex.png", width = 800, res = 100)
+ggplot(df2_ex, aes(x = Cohort, color = Scenario)) +
+  geom_smooth(aes(y = Perc), method = "loess", se = TRUE) +
+  geom_point(aes(y = Perc, alpha = 0.1)) +
+  facet_wrap(~ Scenario, ncol = 3) +
+  labs(x = "Cohort", y = "Percentile of the child rankings") +
+  theme_light() + theme(legend.position="none", strip.text.x = element_text(size = 10, colour = "black"))+
+  theme(strip.background =element_rect(fill="#e6e5e3"))+ 
+  scale_color_manual(values=c('#636EFA', '#EF553B', '#00CC96'))
+dev.off()
 
 # Per Scenario (Zoomed Plots)
 ggplot(result2_s1, aes(x = Cohort, y= mean.rk./n)) +
